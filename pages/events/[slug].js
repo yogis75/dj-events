3f43dcc32c4a846ded77c70af242a853e34a0c58
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-08-25 08:12:13 +0530

    1.Pages and Routes

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function SlugPage() {
+  return (
+    <div>
+      <h1>Slug 1</h1>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AppEventPage() {
+  return (
+    <div>
+      <h1>Add Event</h1>
+    </div>
+  );
+}
