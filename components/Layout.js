fe881505d6efa7ffbf48fffc17fbd1f61c95d967
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-08-25 16:41:02 +0530

    4. 404 & Aliasing with jsconfig

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
-import styles from "../styles/Layout.module.css";
+import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
   return (
