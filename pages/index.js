d1f2e10679f15221ad27d861fff9638b1190f36d
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-08-27 08:57:53 +0530

    6. Fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/Config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
